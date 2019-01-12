c94d2ff515e7b37e80743e69def91faaff32e02b
luyongwen <luyongwen@hotmail.com>
2019-01-12 14:17:48 +0800

    DrawDigit

---
+++ DrawDigit.py
@@ -0,0 +1,21 @@
+# This is a program to draw a digit.
+import turtle
+def drawline(draw):
+    turtle.pendown() if draw else turtle.penup()
+    turtle.fd(40)
+    turtle.right(90)
+
+def drawdigit(digit):
+    drawline(True) if digit in [2, 3, 4, 5, 6, 8, 9] else drawline(False)
+    drawline(True) if digit in [0, 1, 3, 4, 5, 6, 7, 8, 9] else drawline(False)
+    drawline(True) if digit in [0, 2, 3, 5, 6, 8] else drawline(False)
+    drawline(True) if digit in [0, 2, 6, 8] else drawline(False)
+    turtle.left(90)
+    drawline(True) if digit in [0, 4, 5, 6, 8, 9] else drawline(False)
+    drawline(True) if digit in [0, 2, 3, 5, 6, 7, 8, 9] else drawline(False)
+    drawline(True) if digit in [0, 1, 2, 3, 4, 7, 8, 9] else drawline(False)
+    
+turtle.pensize(5)
+turtle.speed("fastest")
+drawdigit(9)
+turtle.done()
