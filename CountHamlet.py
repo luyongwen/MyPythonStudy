6915bccc749afd25ae7a3bd7d0cb8a61f8ec7cc2
luyongwen <luyongwen@hotmail.com>
2019-01-21 10:21:49 +0800

    hamlet

---
+++ CountHamlet.py
@@ -0,0 +1,25 @@
+# This is a program for count words in hamlet.
+def getTxt():
+    txt = open("hamlet.txt", "r").read()
+    txt = txt.lower()
+    for ch in "~!@#$%^&*()_+{}|:<>?`-=[];',./":
+        txt = txt.replace(ch, " ")
+    return txt
+
+
+def countWords(ls):
+    count = dict()
+    for word in ls:
+        count[word] = count.get(word, 0) + 1
+    return count
+
+def tranTxt(str):
+    str = str.split()
+    return str
+
+
+hamletTxt = getTxt()
+dic = countWords(tranTxt(getTxt()))
+item = list(dic.items())
+item.sort(key=lambda x:x[1], reverse=True)
+print(item)
