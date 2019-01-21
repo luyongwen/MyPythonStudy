325a09f56103972ef48c66f02e8d679a63bb9772
luyongwen <luyongwen@hotmail.com>
2019-01-21 12:05:38 +0800

    something

--- ScoreClassfy.py
+++ ScoreClassfy.py
@@ -9,6 +9,6 @@ try:
         grade ="C"
     elif score >= 60:
         grade ="D"
-    print("Your score class is " + grade, end=".")
+    print("Your score class is " + grade, end=".\n")
 except:
     print("Input error!")
