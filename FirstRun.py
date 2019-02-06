73710d65e428b811bd6057d1f3e4a455b60d3da4
luyongwen <luyongwen@hotmail.com>
2019-02-06 21:21:51 +0800

    Linux.run

--- FirstRun.py
+++ FirstRun.py
@@ -1,6 +1,7 @@
 # 该脚本适用于Windows，Ubuntu系统，为其他脚本搭建运行环境。
 import os
 import platform
+import sys
 # 判断Windows还是Ubuntu，返回系统名称。
 def decide_system():
     system = platform.system().lower()
@@ -18,35 +19,14 @@ def decide_system():
 
 # 搭建Ubuntu系统环境。
 def system_enviroment():
-    os.system("apt update && apt upgrade -y && apt install python3.6 python-dev -y python3-pip")
-
-
-class Setup:
-    def __init__(self,  system_name, target_list):
-        system_name = system_name
-        target_list = target_list
-        if system_name == "Windows":
-            tools = "pip"
-        elif system_name in ["CentOS", "Redhat"]:
-            tools = ""
-
-    def setup(system_name, target_list):
-        pass
-
-def setup(target):
-    result = os.system("pip install " + target)
+    result = os.system("apt update && apt upgrade -y && apt install python3.6 python3-dev -y python3-pip")
     if result:
-        fail.append(target)
-    else:
-        success.append(target)
-
-
-def main(ls):
-    for target in ls:
-        setup_result(target)
+        print("网络错误无法更新系统环境，程序退出，请尝试重新运行程序")
+        sys.exit()
 
 
 def print_result(fail, success):
+
     if len(fail) == 0:
         print("All package are installed.")
     else:
@@ -58,7 +38,27 @@ def print_result(fail, success):
             print(s, end=" ")
         print("\n")
 
-ls = ["pillow", "jieba", "pyinstaller", "requests", "scipy", "numpy", "matplotlib", "wordcloud"]
-fail = []
-success = []
-print(decide_system())
+
+
+def main():
+    system_name = decide_system()
+    print(system_name)
+    if system_name == "Windows":
+        tool = "pip"
+    elif system_name == "Ubuntu":
+        system_enviroment()
+        tool = "pip3"
+    else:
+        print("抱歉本程序不适合在该操作系统执行，程序退出")
+        sys.exit()
+    ls = ["pillow", "jieba", "pyinstaller", "requests", "scipy", "numpy", "matplotlib", "wordcloud"]
+    fail = []
+    success = []
+    for target in ls:
+        result = os.system(tool + " install " + target)
+        if result:
+            fail.append(target)
+        else:
+            success.append(target)
+main()
+print(print_result)
