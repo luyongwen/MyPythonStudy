51a094ddcbdd06b974f5158252fa080b41b854a6
yongwen lu <luyongwen@hotmail.com>
2019-02-06 10:12:44 +0800

    wordcloud

--- TestForWordCloud.py
+++ TestForWordCloud.py
@@ -2,10 +2,11 @@
 import jieba
 import wordcloud
 import scipy.misc
-mask = scipy.misc.imread("wujiaoxing.jpg")
+file_name = "wujiaoxing.jpg"
+mask = scipy.misc.imread(file_name)
 string = open("三国.txt", "rt", encoding="utf-8").read()
 ls = jieba.lcut(string)
 txt = " ".join(ls)
 w = wordcloud.WordCloud(font_path="msyh.ttc", background_color="white", mask=mask)
 w.generate(txt)
-w.to_file("C:\\Users\\LYW\\Pictures\\hamlet.gif")
+w.to_file("/Users/LYW/Pictures/" + file_name)
