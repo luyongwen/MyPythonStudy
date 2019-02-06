6d7bc3c45ca4e256e8d56412d773a94d8e1f2ee6
yongwen lu <luyongwen@hotmail.com>
2019-02-06 16:50:42 +0800

    enviroment

--- FirstRun.py
+++ FirstRun.py
@@ -1,9 +1,49 @@
 # Before running other programs, please run this script.
 import os
-ls = ["pillow", "jieba", "pyinstaller", "requests", "scipy", "numpy", "matplotlib", "wordcloud"]
-fail = []
-success = []
-def setup_result(target):
+import platform
+# 判断Windows还是Linux，返回系统名称。
+def decide_system():
+    system = platform.system().lower()
+    if system == "Linux".lower():
+        decide_Linux()
+    elif system == "Windows".lower():
+        return "Windows"
+    else:
+        return "Unkown System"
+
+# 判断Linux发行版，返回发行版名称。
+def decide_Linux():
+    release = os.popen("lsb_release -a").read().lower()
+    if "ubuntu" in release:
+        return "Ubuntu"
+    elif "centos" in release:
+        return "CentOS"
+    elif "redhat" in release:
+        return "Redhat"
+    else:
+        return "Unknown System"
+
+# 搭建Linux系统环境。
+def system_enviroment(system_name):
+    if system_name in ["Redhat", "CentOS"]:
+        package_tool = "yum"
+    else:
+        package_tool = "apt"
+    os.system(package_tool + " install -y " + py_release)
+
+class Setup:
+    def __init__(self,  system_name, target_list):
+        system_name = system_name
+        target_list = target_list
+        if system_name == "Windows":
+            tools = "pip"
+        elif system_name in ["CentOS", "Redhat"]:
+            tools = ""
+
+    def setup(system_name, target_list):
+        pass
+
+def setup(target):
     result = os.system("pip install " + target)
     if result:
         fail.append(target)
@@ -28,6 +68,7 @@ def print_result(fail, success):
             print(s, end=" ")
         print("\n")
 
-
-main(ls)
-print_result(fail, success)
+ls = ["pillow", "jieba", "pyinstaller", "requests", "scipy", "numpy", "matplotlib", "wordcloud"]
+fail = []
+success = []
+print(decide_system())
