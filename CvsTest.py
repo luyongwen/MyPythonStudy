6497a16d4a3e8057890ff64967d948284993db2f
luyongwen <luyongwen@hotmail.com>
2019-02-09 09:27:17 +0800

    csv

--- CvsTest.py
+++ CvsTest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import csv
-f = open("Csv.csv", "w", newline="")
+f = open("Csv.csv", "w", newline="", encoding="utf-8")
 writer = csv.writer(f)
-writer.writerow("hello")
+writer.writerow(["hello", "python"])
 f.close
